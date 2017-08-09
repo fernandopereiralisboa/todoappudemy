fe37f38cd239e3bda248d1abe110ea14e33e3429
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2017-08-09 18:26:02 -0300

    OnGoing: Created Frontend

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
+
+export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name='tarefas' small='Cadastro'>
+                </PageHeader>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
+                <TodoList />
+            </div>
+        )
+    }
+}
